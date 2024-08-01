d88f6dd8c10b2135f9cf284fea8ccb5285fcee5d
araujo-p4 <yago-araujo@hotmail.com>
2024-07-31 22:59:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ function daysleft(firstDate, secondDate){
 }
 
 function update(){
-    var currentDate = new Date() 
-    var tomorrowDate = new Date(2024, 10, 11)
-    const firstDate = currentDate
-    const secondDate = tomorrowDate
+    var currentDate = new Date()
+    var year = currentDate.getFullYear()
+    var month = currentDate.getMonth()+1
+    var day = currentDate.getDate()
+    console.log(year)
+    console.log(month)
+    console.log(day)
+    const firstDate = new Date(year, month, day)
+    const secondDate = new Date(2024, 10, 11)
     var diffDays
     diffDays = daysleft(firstDate, secondDate)
     let countDays = document.getElementById("count-days")
     countDays.innerText = diffDays
 }
 
-function send(){
-    let textSay = document.getElementById("say").value
-    let savedMessage = document.getElementById("saved-message")
-    savedMessage.textContent += textSay + "\n";
-}
+
 
